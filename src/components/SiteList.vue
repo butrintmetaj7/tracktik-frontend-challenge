8dd99b1015fc6249b5563fc686d0acfcb92aad60
butrint.metaj <144210576+butrintmetaj7@users.noreply.github.com>
2024-07-22 02:17:48 +0200

    Implement search in FE

--- src/components/SiteList.vue
+++ src/components/SiteList.vue
@@ -1,21 +1,39 @@
 <template>
   <div>
-    <div v-for="site in sites" :key="site.id" class="site-item">
+    <div v-for="site in filteredSites" :key="site.id" class="site-item">
       <SiteCard :site="site"/>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
-import {computed, onMounted} from 'vue';
-import {useSiteStore} from "@/stores/site";
+import { computed, onMounted, defineProps } from 'vue';
+import { useSiteStore } from '@/stores/site';
 import SiteCard from './SiteCard.vue';
+
+const props = defineProps({
+  searchQuery: {
+    type: String,
+    required: true
+  }
+});
 
 const siteStore = useSiteStore();
 
-
 const sites = computed(() => siteStore.sites);
+
+const filteredSites = computed(() => {
+  if (!props.searchQuery) {
+    return sites.value;
+  }
+  return sites.value.filter(site =>
+      site.title.toLowerCase().includes(props.searchQuery.toLowerCase()) ||
+      site.address.street.toLowerCase().includes(props.searchQuery.toLowerCase()) ||
+      site.address.city.toLowerCase().includes(props.searchQuery.toLowerCase()) ||
+      site.contacts.main.firstName.toLowerCase().includes(props.searchQuery.toLowerCase()) ||
+      site.contacts.main.lastName.toLowerCase().includes(props.searchQuery.toLowerCase())
+  );
+});
 
 onMounted(() => {
   siteStore.fetchSites();
--- src/views/Sites/Index.vue
+++ src/views/Sites/Index.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="sites">
     <h1>All Sites</h1>
-    <SiteList/>
+    <input v-model="searchQuery" placeholder="Search sites..." class="search-input" />
+    <SiteList :search-query="searchQuery"/>
   </div>
 </template>
 
 <script setup lang="ts">
 import SiteList from "@/components/SiteList.vue";
+import  { ref} from "vue";
 
+const searchQuery = ref('');
 </script>
+<style>
+.search-input {
+  margin-bottom: 20px;
+  padding: 10px;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+</style>
